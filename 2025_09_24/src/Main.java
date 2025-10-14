c2e8bcfff551de41818d115a0a619391db18ae27
Nikodem Jokiel <medokin78@gmail.com>
2025-10-14 22:30:46 +0200

    zad 4.2, 4.3a, 4.3b

--- 2025_09_24/src/Main.java
+++ 2025_09_24/src/Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -27,38 +29,122 @@ public class Main {
         System.out.println(counter);
     }
 
-    public static void PrimeFactors(Scanner sc) { //jeszcze nie działa
+    public static void PrimeFactors(Scanner sc) {
         int mostFactors = 0;
+        int mostDifferentFactors = 0;
         int result1 = 0;
+        int result2 = 0;
         while (sc.hasNext()) {
             int n = Integer.parseInt(sc.next());
-
-            int factorsCount = 1;
-            for (int i = 2; n >= 0; i++) {
-                while (n % i == 0) {
+            int temp = n;
+            int factorsCount = 0;
+            int differentFactorsCount = 0;
+            int div = 2;
+            while(temp > 1){
+                boolean hasFactor = false;
+                while(temp % div == 0){
                     factorsCount++;
-                    n /= i;
+                    temp = temp / div;
+                    if(!hasFactor){
+                        differentFactorsCount++;
+                    }
+                    hasFactor = true;
                 }
+                div++;
+
             }
-            if(factorsCount >= mostFactors){
+            if(factorsCount > mostFactors){
                 mostFactors = factorsCount;
                 result1 = n;
             }
+            if(differentFactorsCount > mostDifferentFactors){
+                mostDifferentFactors = differentFactorsCount;
+                result2 = n;
+            }
         }
-        System.out.println(mostFactors);
-        System.out.println(result1);
+        System.out.println(result1 + " " + mostFactors);
+        System.out.println(result2 + " " + mostDifferentFactors);
     }
+
+    public static void Triple(Scanner sc) {
+        List<Integer> numbers = new ArrayList<>();
+        while(sc.hasNext()) {
+            numbers.add(sc.nextInt());
+        }
+        int size = numbers.size();
+        int counter = 0;
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(numbers.get(j) % numbers.get(i) == 0 && numbers.get(i) != numbers.get(j)){
+                    for(int k = 0; k < size; k++){
+                        if(numbers.get(k) % numbers.get(j) == 0 && numbers.get(j) != numbers.get(k)){
+                            System.out.println(numbers.get(i) + " " + numbers.get(j) + " " + numbers.get(k));
+                            counter++;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("number of triples: " + counter);
+    }
+
+    public static void Fifths(Scanner sc) {
+        List<Integer> numbers = new ArrayList<>();
+        while(sc.hasNext()) {
+            numbers.add(sc.nextInt());
+        }
+        int size = numbers.size();
+        int counter = 0;
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(numbers.get(j) % numbers.get(i) == 0 && numbers.get(i) != numbers.get(j)){
+                    for(int k = 0; k < size; k++){
+                        if(numbers.get(k) % numbers.get(j) == 0 && numbers.get(j) != numbers.get(k)){
+                            for(int l = 0; l < size; l++){
+                                if(numbers.get(l) % numbers.get(k) == 0 && numbers.get(k) != numbers.get(l)){
+                                    for(int m = 0; m < size; m++){
+                                        if(numbers.get(m) % numbers.get(l) == 0 && numbers.get(l) != numbers.get(m)){
+                                            System.out.println(numbers.get(i) + " " + numbers.get(j) + " " + numbers.get(k) + " " + numbers.get(l) + " " + numbers.get(m));
+                                            counter++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("number of fifths: " + counter);
+    }
+
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\student\\IdeaProjects\\2025_08_24\\Dane_2205\\liczby.txt");
+        File file = new File("C:\\Users\\Nikodem\\Documents\\GitHub\\PPAM\\2025_09_24\\Dane_2205\\liczby.txt"); //chanage your local path
         Scanner sc = null;
+        Scanner sc2 = null;
+        Scanner sc3 = null;
+        Scanner sc4 = null;
         try {
             sc = new Scanner(file);
+            sc2 = new Scanner(file);
+            sc3 = new Scanner(file);
+            sc4 = new Scanner(file);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
+
+        System.out.println("zad4.1");
         FirstLastDigit(sc);
-        PrimeFactors(sc);//jeszcze nie działa
+        System.out.println("\n");
+        System.out.println("zad4.2");
+        PrimeFactors(sc2);
+        System.out.println("\n");
+        System.out.println("zad4.3a");
+        Triple(sc3);
+        System.out.println("\n");
+        System.out.println("zad4.3b");
+        Fifths(sc4);
 
     }
 }
